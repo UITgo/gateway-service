af8f5359d2ec7e5e834d4c861e3dd2e3b0589dd9
korobe0906 <laptrinh0906@gmail.com>
2025-11-28 14:54:00 +0700

    fix:jwt

--- src/jwt-auth.guard.ts
+++ src/jwt-auth.guard.ts
@@ -1,4 +1,8 @@
-import { Injectable, ExecutionContext, SetMetadata } from '@nestjs/common';
+import {
+  Injectable,
+  ExecutionContext,
+  SetMetadata,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Reflector } from '@nestjs/core';
 
@@ -6,19 +10,22 @@ export const IS_PUBLIC_KEY = 'isPublic';
 export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
 
 @Injectable()
-export class CognitoAuthGuard extends AuthGuard('jwt') {
-  constructor(private reflector: Reflector) {
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private readonly reflector: Reflector) {
     super();
   }
 
   canActivate(context: ExecutionContext) {
-    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
-      context.getHandler(),
-      context.getClass(),
-    ]);
+    // Cho phép bỏ qua auth nếu handler/class có @Public()
+    const isPublic = this.reflector.getAllAndOverride<boolean>(
+      IS_PUBLIC_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+
     if (isPublic) {
       return true;
     }
+
     return super.canActivate(context);
   }
 }
